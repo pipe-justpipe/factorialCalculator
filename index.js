168ae6a139d1866bcbfd3831f12a48894dc4bd81
pipe-justpipe <aremopipeloluwa@gmail.com>
2023-12-06 09:08:26 +0100

    updated script

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 //Factorial Calculator:Write a function that calculates the factorial of a given number. 
 //DO NOT USE PROMPT, I WANT ONLY 1 CONSOLE.LOG, YOUR FUNCTION MUST BE REUSEABLE, AND MUST RETURN A VALUE
 function calaulateFactorial(number){
+    if (number < 0) {
+    
+        return "please provide a positive integer";
+      }
+    
     let factorial =1;
     for (let i = 2; i <= number; i++){
         factorial *=i;
@@ -9,4 +14,4 @@ function calaulateFactorial(number){
 }console.log(calaulateFactorial(5))
 //reusablity test
 console.log(calaulateFactorial(4))
-console.log(calaulateFactorial(3))
+console.log(calaulateFactorial(-3))
